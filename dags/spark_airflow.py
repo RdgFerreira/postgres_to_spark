e488eb2f49f2bde7982e97d71026e34dfd6027ee
RdgFerreira <araujofro@gmail.com>
2025-10-23 08:43:26 -0300

    added hostname to spark master; added commands to makefile and changed approach in dag definition

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -4,34 +4,56 @@ from datetime import datetime, timedelta
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-dag = DAG(
-    dag_id = "spark_postgres_job",
-    default_args = {
-        "owner": "airflow",
-    },
-    start_date=datetime(2024, 1, 1),
-    schedule=timedelta(days=1)
-)
+# dag = DAG(
+#     dag_id = "spark_postgres_job",
+#     default_args = {
+#         "owner": "airflow",
+#     },
+#     start_date=datetime(2024, 1, 1),
+#     schedule=timedelta(days=1)
+# )
 
-start = PythonOperator(
-    task_id = "start",
-    python_callable = lambda: print("Starting Spark Postgres Job DAG"),
-    dag = dag,
-)
+# start = PythonOperator(
+#     task_id = "start",
+#     python_callable = lambda: print("Starting Spark Postgres Job DAG"),
+#     dag = dag,
+# )
 
-spark_job = SparkSubmitOperator(
-    task_id = "spark_postgres_job_task",
-    application = "/jobs/simple_job.py",
-    conn_id = "spark_conn",
-    # application_args = [],
-    verbose = True,
-    dag = dag,
-)
+# spark_job = SparkSubmitOperator(
+#     task_id = "spark_postgres_job_task",
+#     application = "/jobs/simple_job.py",
+#     conn_id = "spark_conn",
+#     # application_args = [],
+#     verbose = True,
+#     dag = dag,
+# )
 
-end = PythonOperator(
-    task_id="end",
-    python_callable = lambda: print("Jobs completed successfully"),
-    dag=dag
-)
+# end = PythonOperator(
+#     task_id="end",
+#     python_callable = lambda: print("Jobs completed successfully"),
+#     dag=dag
+# )
 
-start >> spark_job >> end
+# start >> spark_job >> end
+
+with DAG("spark_postgres_job",
+          default_args={
+              "owner": "airflow",
+              "depends_on_past": False,
+          },
+          start_date=datetime(2024, 1, 1),
+          schedule=timedelta(days=1)) as dag:
+    
+    spark_job = SparkSubmitOperator(
+        task_id="spark_postgres_job_task",
+        application="/jobs/simple_job.py",
+        conn_id="spark_default",
+        total_executor_cores=1,
+        executor_cores=1,
+        executor_memory="1g",
+        num_executors=1,
+        driver_memory="1g",
+        verbose=True,
+    )
+
+spark_job
--- makefile
+++ makefile
@@ -13,11 +13,18 @@ down_up:
 clean:
 	docker compose down && docker system prune -f && docker image prune -f && docker volume prune -f
 
-clean_build_restart: 
-	make clean && make build && make clean && make up
+clean_delete_volumes:
+	docker compose down -v && docker system prune -f && docker image prune -f && docker volume prune -f
 
 clean_restart:
 	make clean && make up
+
+clean_build_restart: 
+	make clean && make build && make clean && make up
+
+clean_all_volumes_restart: 
+	make clean_delete_volumes && make clean_restart
+
 
 remove_all_volumes:
 	docker volume rm $(docker volume ls -qf dangling=true)
@@ -37,3 +44,4 @@ submit:
 	docker exec spark_master spark-submit --master spark://spark-master:7077 --deploy-mode client ./$(app)
 
 # Inside scheduler: airflow dags reserialize to visualize errors in dag python files
+# docker exec scheduler airflow dags reserialize
