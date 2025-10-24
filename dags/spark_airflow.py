2561453495be47b03be48c030ee03c5702797c59
RdgFerreira <araujofro@gmail.com>
2025-10-24 16:55:24 -0300

    Expose ports in Dockerfile; Standalone airflow setup change to docker compose; adjust app path in dag definition

--- dags/spark_airflow.py
+++ dags/spark_airflow.py
@@ -4,38 +4,6 @@ from datetime import datetime, timedelta
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-# dag = DAG(
-#     dag_id = "spark_postgres_job",
-#     default_args = {
-#         "owner": "airflow",
-#     },
-#     start_date=datetime(2024, 1, 1),
-#     schedule=timedelta(days=1)
-# )
-
-# start = PythonOperator(
-#     task_id = "start",
-#     python_callable = lambda: print("Starting Spark Postgres Job DAG"),
-#     dag = dag,
-# )
-
-# spark_job = SparkSubmitOperator(
-#     task_id = "spark_postgres_job_task",
-#     application = "/jobs/simple_job.py",
-#     conn_id = "spark_conn",
-#     # application_args = [],
-#     verbose = True,
-#     dag = dag,
-# )
-
-# end = PythonOperator(
-#     task_id="end",
-#     python_callable = lambda: print("Jobs completed successfully"),
-#     dag=dag
-# )
-
-# start >> spark_job >> end
-
 with DAG("spark_postgres_job",
           default_args={
               "owner": "airflow",
@@ -46,7 +14,7 @@ with DAG("spark_postgres_job",
     
     spark_job = SparkSubmitOperator(
         task_id="spark_postgres_job_task",
-        application="/jobs/simple_job.py",
+        application="/app/simple_job.py", # app or jobs/simple_job.py if deployed as external client
         conn_id="spark_default",
         total_executor_cores=1,
         executor_cores=1,
